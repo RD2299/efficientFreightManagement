35aea0bfa24828db89f44019818a8dd0d06c8662
RD <rupam25789das@gmail.com>
2022-09-23 22:28:48 +0530

    docs: add create, update functions and rest APIs for admin page

--- src/back-end/springboot-backend_springboot-backend/src/main/java/project/dac2022/springboot/controller/AdminProfileController.java
+++ src/back-end/springboot-backend_springboot-backend/src/main/java/project/dac2022/springboot/controller/AdminProfileController.java
@@ -3,8 +3,13 @@ package project.dac2022.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,5 +54,46 @@ public class AdminProfileController {
 	{
 		return A_PS.getAllAdmins();
 	}
+	
+	@PostMapping
+	@RequestMapping("/createcustomer")
+	public ResponseEntity<?> CustomerCreation(@RequestBody CustomerRegister cr)
+	{
+		return A_PS.createCustomer(cr);
+	}
+	
+	@PostMapping
+	@RequestMapping("/createdriver")
+	public ResponseEntity<?> DriverCreation(@RequestBody DriverRegister dr)
+	{
+		return A_PS.createDriver(dr);
+	}
+	
+	@PostMapping
+	@RequestMapping("/createtrucklendor")
+	public ResponseEntity<?> TruckLendorCreation(@RequestBody TruckLendorRegister tlr)
+	{
+		return A_PS.createTruckLendor(tlr);
+	}
+	
+	
+	@PutMapping("/updateCustomer/{c_ID}")
+	public ResponseEntity<CustomerRegister> updateCustomer(@PathVariable long c_ID,@RequestBody CustomerRegister c_R)
+	{
+		return A_PS.updateCustomer(c_ID,c_R);
+	}
+	
+	
+	public ResponseEntity<DriverRegister> updateCustomer(long d_ID, DriverRegister d_R)
+	{
+		return A_PS.updateDriver(d_ID,d_R);
+	}
+	
+	
+	public ResponseEntity<TruckLendorRegister> updateTruckLendor(long tl_ID, TruckLendorRegister tl_R)
+	{
+		return A_PS.updateTruckLendor(tl_ID,tl_R);
+	}
+
 	
 }
--- src/back-end/springboot-backend_springboot-backend/src/main/java/project/dac2022/springboot/service/AdminProfileService.java
+++ src/back-end/springboot-backend_springboot-backend/src/main/java/project/dac2022/springboot/service/AdminProfileService.java
@@ -3,6 +3,8 @@ package project.dac2022.springboot.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 //import org.springframework.http.HttpStatus;
 //import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -64,7 +66,74 @@ public class AdminProfileService {
 	}
 	
 	
+	public ResponseEntity<?> createCustomer(CustomerRegister cr) {
+		// TODO Auto-generated method stub
+		
+		try {
+			C_R.save(cr);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
+	}
 	
+	
+	public ResponseEntity<?> createDriver(DriverRegister dr) {
+		// TODO Auto-generated method stub
+		
+		try {
+			D_R.save(dr);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
+	}
+	
+	
+	public ResponseEntity<?> createTruckLendor(TruckLendorRegister tlr) {
+		// TODO Auto-generated method stub
+		
+		try {
+			TL_R.save(tlr);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
+	}
+
+
+	public ResponseEntity<CustomerRegister> updateCustomer(long c_ID, CustomerRegister c_R2) {
+		// TODO Auto-generated method stub
+		
+		CustomerRegister updateCustomer = C_R.findById(c_ID).orElseThrow();
+		
+		
+		updateCustomer.setC_FirstName(c_R2.getC_FirstName());
+		updateCustomer.setC_MiddleName(c_R2.getC_MiddleName());
+		updateCustomer.setC_LastName(c_R2.getC_LastName());
+		updateCustomer.setC_Email(c_R2.getC_Email());
+		updateCustomer.setC_Phone(c_R2.getC_Phone());
+		updateCustomer.setC_Address(c_R2.getC_Address());
+		C_R.save(updateCustomer);
+		
+		return ResponseEntity.ok(updateCustomer);
+	}
+
+
+	public ResponseEntity<DriverRegister> updateDriver(long d_ID, DriverRegister d_R2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public ResponseEntity<TruckLendorRegister> updateTruckLendor(long tl_ID, TruckLendorRegister tl_R2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	
 	
 }
